dfe1c1817285b3b8e2a535cfe17f247ed09f2796
AnitaBalasyan <mariam.abrahamyan.88@mail.ru>
2022-10-16 09:24:30 +0400

    Create quiz app

---
+++ script.js
@@ -0,0 +1,100 @@
+const h2 = document.querySelector("h2");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const div = document.querySelector(".absalute");
+const input1 =document.querySelector(".limit")
+
+const limit = 3;
+
+const createQuestionsApp = (category) => {
+  const promise = fetch(
+    `https://quizapi.io/api/v1/questions?apiKey=XEtdSWA2te2Uj1Isz2vMQLqC8ScgrSgRxUd3Gvi8&limit=${limit}&category=${category}`
+  );
+
+  const showQuestions = (question) => {
+    h2.innerHTML = `
+      ${question.question}
+    `;
+
+    form.innerHTML = `
+      <label>
+      <input type="radio" name="answer" value="answer_a_correct" style="background-color: black;"  />
+      ${question.answers.answer_a}
+      </label>
+      <label>
+      <input type="radio" name="answer" value="answer_b_correct" />
+      ${question.answers.answer_b}
+      </label>
+      <label><br/>
+      <input type="radio" name="answer" value="answer_c_correct" />
+      ${question.answers.answer_c}
+      </label>
+      <label>
+      <input type="radio" name="answer" value="answer_d_correct" />
+      ${question.answers.answer_d}
+      </label>
+      <button type="submit">Next question</button>
+  `;
+  };
+
+  promise
+    .then((response) => {
+      if (response.ok === false) {
+        return Promise.reject(response);
+      }
+      return response.json();
+    })
+    .then((questions) => {
+      let currentQuestionIndex = 0;
+
+      let wrongAnswer = 0;
+      let rightAnswer = 0;
+
+      showQuestions(questions[currentQuestionIndex]);
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const inputs = document.querySelectorAll("input");
+
+        inputs.forEach((input) => {
+          if (!input.checked) return;
+
+          if (questions[0].correct_answers[input.value] === "true")
+            rightAnswer++;
+          else wrongAnswer++;
+
+          currentQuestionIndex++;
+
+          if (currentQuestionIndex === limit) {
+            alert(`Wrong Answers: ${wrongAnswer}`);
+            alert(`Right Answers: ${rightAnswer}`);
+
+            window.location.reload();
+
+            return;
+          }
+
+          showQuestions(questions[currentQuestionIndex]);
+        });
+      });
+    })
+    .catch((error) => {
+      console.log(error, "Error");
+    });
+};
+
+button.addEventListener("click", () => {
+  createQuestionsApp(input.value);
+  div.style.display = "block";
+  if(currentQuestionIndex >= 0) {
+    
+  }
+  
+});
+
+
+
+
+
